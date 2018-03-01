b5d08bd0b61eacb839da3c801f27fde1fb46743a
Ralph Bean <rbean@redhat.com>
2018-02-28 20:14:33 -0500

    Make a note of new config values in the config files.

--- fedmsg.d/resultsdb.py
+++ fedmsg.d/resultsdb.py
@@ -1,3 +1,5 @@
 config = dict(
-    resultsdb_handler=True
+    resultsdb_handler=True,
+    # Topic on which greenwave should listen for new resultsdb results.
+    resultsdb_topic_suffix='taskotron.result.new',
 )
--- fedmsg.d/waiverdb.py
+++ fedmsg.d/waiverdb.py
@@ -1,3 +1,5 @@
 config = dict(
-    waiverdb_handler=True
+    waiverdb_handler=True,
+    # Topic on which greenwave should listen for new waiverdb waivers.
+    waiverdb_topic_suffix='waiver.new',
 )
