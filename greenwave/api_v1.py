release-engineering/greenwave

commit 6e235967513b534182e3d24771429a852c816f46

Retrieve multiple waivers at once
Omits doing multiple HTTP requests to WaiverDB.

greenwave/api_v1.py
@@ -248,18 +248,17 @@ def make_decision():
     subjects = [item for item in data['subject'] if isinstance(item, dict)]
     if not subjects:
         raise BadRequest('Invalid subject, must be a list of dicts')
-    answers = []
 
-    all_results, all_waivers = [], []
+    waivers = retrieve_waivers(product_version, subjects)
+    waivers = [w for w in waivers if w['id'] not in ignore_waivers]
+
+    results = []
     for item in subjects:
-        results = retrieve_results(item)
-        results = [r for r in results if r['id'] not in ignore_results]
-        all_results.extend(results)
+        results.extend(retrieve_results(item))
+    results = [r for r in results if r['id'] not in ignore_results]
 
-        waivers = retrieve_waivers(product_version, item)
-        waivers = [w for w in waivers if w['id'] not in ignore_waivers]
-        all_waivers.extend(waivers)
-
+    answers = []
+    for item in subjects:
         for policy in applicable_policies:
             answers.extend(policy.check(item, results, waivers))
 
@@ -272,8 +271,8 @@ def make_decision():
     }
     if verbose:
         res.update({
-            'results': all_results,
-            'waivers': all_waivers,
+            'results': results,
+            'waivers': waivers,
         })
     resp = jsonify(res)
     resp = insert_headers(resp)

greenwave/resources.py
@@ -80,13 +80,15 @@ def retrieve_results(item):
 
 # NOTE - not cached, for now.
 @greenwave.utils.retry(wait_on=urllib3.exceptions.NewConnectionError)
-def retrieve_waivers(product_version, item):
+def retrieve_waivers(product_version, items):
     timeout = current_app.config['REQUESTS_TIMEOUT']
     verify = current_app.config['REQUESTS_VERIFY']
+
     data = {
         'product_version': product_version,
-        'results': [{"subject": item}]
+        'results': [{"subject": item} for item in items]
     }
+
     response = requests_session.post(
         current_app.config['WAIVERDB_API_URL'] + '/waivers/+by-subjects-and-testcases',
         headers={'Content-Type': 'application/json'},

commit 893dc0310c41232edf804f88bd2f3590a294af79

Fix checking policies for specific item
Results and waivers that Policy.check() takes as arguments should all be
related to given item.

This was broken in 6e23596 by passing results and waivers for all items.

greenwave/api_v1.py
@@ -253,17 +253,18 @@ def make_decision():
     waivers = [w for w in waivers if w['id'] not in ignore_waivers]
 
     results = []
-    for item in subjects:
-        results.extend(retrieve_results(item))
-    results = [r for r in results if r['id'] not in ignore_results]
-
     answers = []
     for item in subjects:
-        relevant_policies = [
-            policy for policy in applicable_policies
-            if policy.is_relevant_to(item)]
-        for policy in relevant_policies:
-            answers.extend(policy.check(item, results, waivers))
+        item_results = retrieve_results(item)
+        item_results = [r for r in item_results if r['id'] not in ignore_results]
+        results.extend(item_results)
+
+        subject_subset = set(item.items())
+        item_waivers = [w for w in waivers if subject_subset <= set(w['subject'].items())]
+
+        for policy in applicable_policies:
+            if policy.is_relevant_to(item):
+                answers.extend(policy.check(item, item_results, item_waivers))
 
     res = {
         'policies_satisfied': all(answer.is_satisfied for answer in answers),