7ef8e5348dd82451db1c98da664efc884d057675
mprahl <mprahl@redhat.com>
2019-01-17 14:01:43 -0500

    Replace PackageSpecificBuild with a packages whitelist on the policy

--- greenwave/tests/test_rules.py
+++ greenwave/tests/test_rules.py
@@ -29,28 +29,6 @@ def test_match_passing_test_case_rule():
     assert not rule.matches(policy, testcase='other_test_case')
 
 
-def test_match_package_specific_rule():
-    policy_yaml = dedent("""
-        --- !Policy
-        id: "some_policy"
-        product_versions: [rhel-9000]
-        decision_context: bodhi_update_push_stable
-        subject_type: koji_build
-        rules:
-          - !PackageSpecificBuild {test_case_name: some_test_case, repos: [nethack]}
-    """)
-    policies = Policy.safe_load_all(policy_yaml)
-    assert len(policies) == 1
-
-    policy = policies[0]
-    assert len(policy.rules) == 1
-
-    rule = policy.rules[0]
-    assert rule.matches(policy)
-    assert rule.matches(policy, testcase='some_test_case')
-    assert not rule.matches(policy, testcase='other_test_case')
-
-
 @mock.patch('greenwave.resources.retrieve_yaml_remote_rule')
 @mock.patch('greenwave.resources.retrieve_scm_from_koji')
 def test_match_remote_rule(mock_retrieve_scm_from_koji, mock_retrieve_yaml_remote_rule):
