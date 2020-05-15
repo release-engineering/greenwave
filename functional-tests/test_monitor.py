6de0b965c7fb2d9b014c9a246486d4fdbf29c73f
Valerij Maljulin <vmaljuli@redhat.com>
2020-05-15 14:14:58 +0200

    Flask fix (ambigous variable name 'l' according to PEP8) More info: https://www.python.org/dev/peps/pep-0008/#names-to-avoid

--- functional-tests/test_monitor.py
+++ functional-tests/test_monitor.py
@@ -14,8 +14,8 @@ def test_metrics(requests_session, greenwave_server):
     r = requests_session.get(greenwave_server + 'api/v1.0/metrics')
 
     assert r.status_code == 200
-    assert len([l for l in r.text.splitlines()
-                if l.startswith('# TYPE')]) >= min_num_of_metrics
+    assert len([line for line in r.text.splitlines()
+                if line.startswith('# TYPE')]) >= min_num_of_metrics
 
 
 def test_standalone_metrics_server_disabled_by_default(requests_session):
@@ -29,5 +29,5 @@ def test_standalone_metrics_server(requests_session):
 
     r = requests_session.get('http://127.0.0.1:10040/metrics')
 
-    assert len([l for l in r.text.splitlines()
-                if l.startswith('# TYPE')]) >= min_num_of_metrics
+    assert len([line for line in r.text.splitlines()
+                if line.startswith('# TYPE')]) >= min_num_of_metrics
